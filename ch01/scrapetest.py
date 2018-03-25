e0ad984c19c2ffcb35d09b115c8b1835dc55a79a
이 규찬 <every1fly@korail.com>
2018-03-25 10:05:16 +0900

    #오류처리개념
    예외상황 다음과 같이 처리

--- ch01/scrapetest.py
+++ ch01/scrapetest.py
@@ -1,5 +1,13 @@
 from urllib.request import urlopen
+from urllib.request import HTTPError
 from bs4 import BeautifulSoup
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), "html.parser")
-print(bsObj.h1)
+
+try:
+	#html = urlopen("http://pythonscraping.com/pages/page1.html")
+	html = urlopen("http://www.pythonscraping.com/pages/error.html")
+except HTTPError as e :
+	print(e)
+	# null을 반환하거나, break 문을 실행하거, 기타 다른 방법을 사용
+else:
+	bsObj = BeautifulSoup(html.read(), "html.parser")
+	print(bsObj.h1)
